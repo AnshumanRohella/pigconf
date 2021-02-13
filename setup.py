0c043db5172dea146ec809922730db6fdf234f52
AnshumanRohella <rohella.anshuman@gmail.com>
2021-02-13 12:22:13 +0900

    Mapper.py
    * Updated string formatting
    * Removed print statements
    * Added documentation comments
    
    README.md
    
    * Added the readme.
    
    setup.py
    
    * Initialized some config

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+
 setup(
     name='pigconf',
     version='0.0.1',
+    author="Anshuman Rohella",
+    author_email="rohella.anshuman@gmail.com",
     descriptoion='A yaml config mapper to create static config classes.',
     py_modules=["mapper"],
     package_dir={'': "src"},
     install_requires=["PyYAML>=5.4.1"],
+    extra_require={
+        "dev": [
+            "pytest>=6.2.2",
+        ],
+    },
 )
